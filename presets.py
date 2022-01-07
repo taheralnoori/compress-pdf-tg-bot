7871a2946de5fe5a902ff089bdbaa5573fab0a3e
MRSAHBE09 <96049133+MRSAHBE09@users.noreply.github.com>
2022-01-07 20:36:13 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,5 +1,5 @@
 class Presets(object):
-    WELCOME_TXT = "<b>Hello.. {}</b>\n<i>I can compress the size of pdf docs. Send me a pdf document to see " \
+    WELCOME_TXT = "<b>Assalamu alaikum Warahmatulla Wabarakatuh.. {}</b>\n<i>I can compress the size of pdf docs. Send me a pdf document to see " \
                   "the magic !</i> "
     INVALID_FORMAT = "<b>Error:</b>\n<i>I can only compress pdf documents. Please make it sure, you have given me a " \
                      "valid document. Try again...</i>"
@@ -11,5 +11,5 @@ class Presets(object):
     FINISHED_DL = "<b>Success !</b>\n<i>Document downloaded successfully.</i>"
     START_COMPRESSING = "⌛️ <b>Processing</b>  ⌛\n<i>Trying to compress the document.</i>"
     FINISHED_JOB = "<b>Success</b> ✅ \n\n<i>Size before job: {}\nSize after job: {}\n\nCredits: </i><a " \
-                   "href='https://github.com/m4mallu'><b> @M4Mallu</b></a> "
+                   "href='https://t.me/south_kashmir'><b> south_kashmir</b></a> "
     JOB_ERROR = "<b>Error:</b>\n<i>Something went wrong ! Process exited</i>"
