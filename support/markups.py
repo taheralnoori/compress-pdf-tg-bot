91c2b9e433bd2eabbb4f46be12e79e9e7f705d25
taheralnoori <82815443+taheralnoori@users.noreply.github.com>
2022-03-23 15:01:30 +0300

    Update markups.py

--- support/markups.py
+++ support/markups.py
@@ -4,15 +4,20 @@ start = [
         [
             InlineKeyboardButton('دعم 💊', url='https://t.me/engineering_electrical9/719?comment=1'),
             InlineKeyboardButton('👨🏻‍💻 مطور', url='https://t.me/ta_ja199')
+        ],
+        [   InlineKeyboardButton('🌟تقييم البوت🌟', url='https://t.me/tlgrmcbot?start=compresspdfbot-review')
         ]
         ]
 
 close = [
         [
             InlineKeyboardButton('دعم 💊', url='https://t.me/engineering_electrical9/719?comment=1'),
-            InlineKeyboardButton('أغلق ❌', callback_data='close_btn')
+            InlineKeyboardButton('👨🏻‍💻 مطور', url='https://t.me/ta_ja199')
+        ],
+        [   InlineKeyboardButton('🌟تقييم البوت🌟', url='https://t.me/tlgrmcbot?start=compresspdfbot-review')
         ]
         ]
+
 
 start_buttons = InlineKeyboardMarkup(start)
 close_button = InlineKeyboardMarkup(close)
